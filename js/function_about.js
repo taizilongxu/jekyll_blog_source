0e48f0cb1164ea9b7ca841f0c3bbe8ff60fff8b1
taizilongxu <468137306@qq.com>
2014-09-13 21:13:23 +0800

    增加about代码打字效果

---
+++ js/function_about.js
@@ -0,0 +1,21 @@
+(function($) {
+	$.fn.typewriter = function() {
+		this.each(function() {
+			var $ele = $(this), str = $ele.html(), progress = 0;
+			$ele.html('');
+			var timer = setInterval(function() {
+				var current = str.substr(progress, 1);
+				if (current == '<') {
+					progress = str.indexOf('>', progress) + 1;
+				} else {
+					progress++;
+				}
+				$ele.html(str.substring(0, progress) + (progress & 1 ? '_' : ''));
+				if (progress >= str.length) {
+					clearInterval(timer);
+				}
+			}, 75);
+		});
+		return this;
+	};
+})(jQuery);
